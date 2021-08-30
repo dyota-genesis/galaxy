70ed85acded700acd1267db8451af0f5fc814af1
Dyota Tanuwibawa <dyota.tanuwibawa@genesisenergies.com>
2021-08-30 14:56:06 +0800

    Move createConnection inside the GET callback

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const app = express();
 
 const mysql = require('mysql');
 
-const conn = mysql.createConnection(
-    {
-        host: 'c8u4r7fp8i8qaniw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-        user: 'pmdpsktr9mw27q90',
-        password: 'hl2dk6g11yemws9o',
-        database: 'tbtl69l4kzu4zjdr'
-    }
-)
+
 
 app.get('/', (req,res) => {
+    
+    const conn = mysql.createConnection(
+        {
+            host: 'c8u4r7fp8i8qaniw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+            user: 'pmdpsktr9mw27q90',
+            password: 'hl2dk6g11yemws9o',
+            database: 'tbtl69l4kzu4zjdr'
+        }
+    )
     
     conn.connect();
 
